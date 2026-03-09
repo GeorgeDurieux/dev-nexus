3aa57fda8fa0d501ab7c1ef1d8fbaa446d0fb530
George Durieux <giorgosdurieux@gmail.com>
2026-03-09 11:01:13 +0200

    browser router base name added

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,40 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home"
-import Projects from "./pages/Projects"
-import Skills from "./pages/Skills"
-import About from "./pages/About"
-import Contact from "./pages/Contact"
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Skills from "./pages/Skills";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
+	return (
+		<BrowserRouter basename="/dev-nexus">
+			<Routes>
+				<Route
+					path="/"
+					element={<Home />}
+				/>
 
-        <Route path="/" element={<Home />} />
+				<Route
+					path="/projects"
+					element={<Projects />}
+				/>
 
-        <Route path="/projects" element={<Projects />} />
+				<Route
+					path="/skills"
+					element={<Skills />}
+				/>
 
-        <Route path="/skills" element={<Skills />} />
+				<Route
+					path="/about"
+					element={<About />}
+				/>
 
-        <Route path="/about" element={<About />} />
-
-        <Route path="/contact" element={<Contact />} />
-
-      </Routes>
-    </BrowserRouter>
-  )
+				<Route
+					path="/contact"
+					element={<Contact />}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
 }
